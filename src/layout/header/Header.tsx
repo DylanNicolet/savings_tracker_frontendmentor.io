7ea0ec0f769415f4f6983f04a6ee198224611df9
Dylan Nicolet <megadylan1@hotmail.com>
2026-07-08 16:29:58 +0200

    [DEV] Integrated header

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -1,9 +1,19 @@
+import logoSmall from '../../assets/images/logo-small.svg';
+import logoLarge from '../../assets/images/logo-large.svg';
+import iconPlus from '../../assets/images/icon-plus.svg';
+
 export default function Header() {
     return(
         <header>
-            <div className="logo">
-                logo
-            </div>
+            <picture>
+                <source srcSet={logoLarge} media="(width >= 768px)" />
+                <img src={logoSmall} alt="Logo of Savings Tracker" />
+            </picture>
+
+            <button className="btn btn--primary">
+                <img src={iconPlus} alt="" aria-hidden="true" />
+                New goal
+            </button>
         </header>
     )
 }
