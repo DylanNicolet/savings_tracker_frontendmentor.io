82b08277ccb2774bc53a56b3e2b9467795280293
Dylan Nicolet <megadylan1@hotmail.com>
2026-07-22 21:28:44 +0400

    [DEV] Integrated Hero bar-chart logic partially

--- src/components/Bar-chart/BarChart.tsx
+++ src/components/Bar-chart/BarChart.tsx
@@ -1,45 +1,134 @@
 import './BarChart.scss';
+import data from '../../../data.json';
 
 export default function BarChart() {
-    const data = [
+
+    let totalDepositAmount = 0;
+
+    let monthlyData = [
         {
-            amount: 400,
-            monthName: 'Aug',
-            barParcentage: 10
-        },
-        {
-            amount: 1150,
-            monthName: 'Sep',
-            barParcentage: 50
-        },
-        {
-            amount: 1149,
-            monthName: 'Oct',
-            barParcentage: 50
-        },
-        {
-            amount: 1550,
-            monthName: 'Nov',
-            barParcentage: 60
-        },
-        {
-            amount: 1950,
-            monthName: 'Dec',
-            barParcentage: 100
-        },
-        {
-            amount: 1025,
+            monthNumber: 1,
+            amount: 0,
             monthName: 'Jan',
-            barParcentage: 40
+            barParcentage: 0,
+        },
+        {
+            monthNumber: 2,
+            amount: 0,
+            monthName: 'Feb',
+            barParcentage: 0,
+        },
+        {
+            monthNumber: 3,
+            amount: 0,
+            monthName: 'Mar',
+            barParcentage: 0,
+        },
+        {
+            monthNumber: 4,
+            amount: 0,
+            monthName: 'Apr',
+            barParcentage: 0,
+        },
+        {
+            monthNumber: 5,
+            amount: 0,
+            monthName: 'May',
+            barParcentage: 0,
+        },
+        { 
+            monthNumber: 6,
+            amount: 0,
+            monthName: 'Jun',
+            barParcentage: 0,
+        },
+        {
+            monthNumber: 7,
+            amount: 0,
+            monthName: 'Jul',
+            barParcentage: 0,
+        },
+        {
+            monthNumber: 8,
+            amount: 0,
+            monthName: 'Aug',
+            barParcentage: 0,
+        },
+        {
+            monthNumber: 9,
+            amount: 0,
+            monthName: 'Sep',
+            barParcentage: 0,
+        },
+        {
+            monthNumber: 10,
+            amount: 0,
+            monthName: 'Oct',
+            barParcentage: 0,
+        },
+        {
+            monthNumber: 11,
+            amount: 0,
+            monthName: 'Nov',
+            barParcentage: 0,
+        },
+        {
+            monthNumber: 12,
+            amount: 0,
+            monthName: 'Dec',
+            barParcentage: 0,
         },
     ];
+
+    function processData() {
+        data.goals.forEach(goal => {
+            goal.deposits.forEach(deposit => {
+                const depositMonthName = new Date(deposit.createdAt).toLocaleString('default', { month: 'short' });
+                const targetMonth = monthlyData.find(month => month.monthName === depositMonthName);
+                
+                // Update total amount
+                totalDepositAmount += deposit.amount;
+
+                if (targetMonth) {
+                    targetMonth.amount += deposit.amount;
+                }
+            });
+        });
+
+        calculatePercentage();
+    }
+
+    function calculatePercentage() {
+        const allAmounts = monthlyData.map(item => item.amount);
+        const highestMonthlyAmount = Math.max(...allAmounts);
+
+        if (highestMonthlyAmount === 0) {
+            monthlyData.forEach(monthData => {
+                monthData.barParcentage = 0;
+            });
+            return;
+        }
+
+        monthlyData.forEach(monthData => {
+            monthData.barParcentage = Math.round((100 / highestMonthlyAmount) * monthData.amount);
+        })
+    } 
+
+    // Continue here next time, find the correct monthNumber
+    function ajustForCurrentMonth() {
+        const thisMonthName = new Date().toLocaleString('default', { month: 'short' });
+
+        const targetMonth = monthlyData.find((month) => month.monthName === thisMonthName);
+    }
+
+    processData()
 
     return (
         <article className='bar-chart card'>
             <h2>Monthly deposits</h2>
 
             <div className='chart'>
-                {data.map((monthly, index) => (
+                {monthlyData.map((monthly, index) => (
                     <div className='monthly' key={index}>
                         <div className="bar-container">
                             <div className='bar' style={{ height: `${monthly.barParcentage}%`}}></div>
